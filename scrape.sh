ce4eea7e02c90dbab4c66aaf4b6e62abf550c4b4
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2025-03-07 14:37:15 +0000

    Add other DSP data URLs

--- scrape.sh
+++ scrape.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 ./download.sh 'https://www.forms.service.gov.uk/performance.json'
+./download.sh 'https://www.notifications.service.gov.uk/features/performance.json'
+./download.sh 'https://raw.githubusercontent.com/alphagov/pay-product-page/refs/heads/main/data/performance.json'
